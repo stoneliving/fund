5bffc61db4bd4fc9cec69a7b04acca620af76ec3
markmiao <290047967@qq.com>
2019-07-24 18:12:10 +0800

    update

--- src/login/index.component.jsx
+++ src/login/index.component.jsx
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import {Input, Button} from 'antd';
+import {Input, Button, Form} from 'antd';
 import State from './index.state';
 import {observer} from 'mobx-react';
 
 @observer
 class Login extends Component {
   render() {
+    const {getFieldDecorator} = this.props.form;
     return (
       <div>
-        <div><Input onChange={e => State.setParam('account', e.target.value)} placeholder="账号"/></div>        
-        <div><Input onChange={e => State.setParam('account', e.target.value)} placeholder="密码"/></div>
+        <Form>
+          <Form.Item>
+            {getFieldDecorator('account',{
+              rules:[{
+                required:true,
+                message:'请输入账号'
+              }]
+            })(
+              <Input onChange={e => State.setParam('account', e.target.value)} placeholder="账号"/>
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password',{
+              rules:[{
+                required:true,
+                message:'请输入密码'
+              }]
+            })(
+              <Input onChange={e => State.setParam('password', e.target.value)} placeholder="密码"/>
+            )}
+          </Form.Item>
+        </Form>
         <div><Button onClick={State.onLogin} type="primary">登录</Button></div>
       </div>
     );
   }
 }
-
+Login = Form.create({})(Login)
 export default Login;
